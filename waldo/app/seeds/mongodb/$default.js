d95ffbb7d646a644c7dc73ecdba665e72ce6333f
Arin Mauk <arin.mauk7@gmail.com>
2017-12-06 01:16:35 -0500

    Dab can't handle a json that contains an attribute it creates itself; companies json has an _id attribute that dab has conflicts with.

--- waldo/app/seeds/mongodb/$default.js
+++ waldo/app/seeds/mongodb/$default.js
@@ -17,16 +17,16 @@ let skills = ["Java","C++","Photoshop","Python","PHP","MySQL","word","excel","ti
 //let companies = ["Apple","Microsoft","IBM","Amazon","Google","Cisco","Intel","Oracle","Qualcomm"]
 let companies = 
 [
-  {"description" : "Technology company most known for their phones.", "name" : "Apple", "location" : "Cupertino, California", "_id" : "5a02944ad05618478a1717dj"},
-  {"description" : "Technology company most known for their gaming console.", "name" : "Microsoft", "location" : "Redmond, Washington", "_id" : "5a02944ad05618478a1717dj"},
-  {"description" : "Technology company most known for their work in AI.", "name" : "IBM", "location" : "Armonk, New York", "_id" : "5a02944ad05618478a1717dj"},
-  {"description" : "Technology company most known for AWS and their online shopping website.", "name" : "Amazon", "location" : "Seattle, Washington", "_id" : "5a02944ad05618478a1717dj"},
-  {"description" : "Technology company most known for their search engine.", "name" : "Google", "location" : "Mountain View, California", "_id" : "5a02944ad05618478a1717dj"},
-  {"description" : "Technology company most known for their IoT and computer security services.", "name" : "Cisco", "location" : "San Jose, California", "_id" : "5a02944ad05618478a1717dj"},
-  {"description" : "Technology company most known for their computer chips.", "name" : "Intel", "location" : "Santa Clara, California", "_id" : "5a02944ad05618478a1717dj"},
-  {"description" : "Technology company most known for their database software.", "name" : "Oracle", "location" : "Redwood Shores, California", "_id" : "5a02944ad05618478a1717dj"},
-  {"description" : "Technology company most known for their computer chips", "name" : "Qualcomm", "location" : "San Diego, California", "_id" : "5a02944ad05618478a1717dj"},
-  {"description" : "Technology company most known for their customer relationship management product.", "name" : "SalesForce", "location" : "San Francisco, California", "_id" : "5a02944ad05618478a1717dj"}
+  {"description" : "Technology company most known for their phones.", "name" : "Apple", "location" : "Cupertino, California", "_idd" : "5a02944ad05618478a1717dj"},
+  {"description" : "Technology company most known for their gaming console.", "name" : "Microsoft", "location" : "Redmond, Washington", "_idd" : "6a02944ad05618478a1717dj"},
+  {"description" : "Technology company most known for their work in AI.", "name" : "IBM", "location" : "Armonk, New York", "_id" : "7a02944ad05618478a1717dj"},
+  {"description" : "Technology company most known for AWS and their online shopping website.", "name" : "Amazon", "location" : "Seattle, Washington", "_idd" : "8a02944ad05618478a1717dj"},
+  {"description" : "Technology company most known for their search engine.", "name" : "Google", "location" : "Mountain View, California", "_idd" : "9a02944ad05618478a1717dj"},
+  {"description" : "Technology company most known for their IoT and computer security services.", "name" : "Cisco", "location" : "San Jose, California", "_idd" : "5a02944ad05618478a1717dj"},
+  {"description" : "Technology company most known for their computer chips.", "name" : "Intel", "location" : "Santa Clara, California", "_idd" : "1a02944ad05618478a1717dj"},
+  {"description" : "Technology company most known for their database software.", "name" : "Oracle", "location" : "Redwood Shores, California", "_idd" : "2a02944ad05618478a1717dj"},
+  {"description" : "Technology company most known for their computer chips", "name" : "Qualcomm", "location" : "San Diego, California", "_idd" : "3a02944ad05618478a1717dj"},
+  {"description" : "Technology company most known for their customer relationship management product.", "name" : "SalesForce", "location" : "San Francisco, California", "_idd" : "4a02944ad05618478a1717dj"}
 ]
 module.exports = {
   //creates 'client', or user.
