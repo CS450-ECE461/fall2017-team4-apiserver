a1344b5818b3a5c27263b71ac9149be60e3f1fcf
Arin Mauk <arin.mauk7@gmail.com>
2017-12-06 00:40:49 -0500

    Added company functionality. Needs more testing

--- waldo/app/seeds/mongodb/$default.js
+++ waldo/app/seeds/mongodb/$default.js
@@ -13,27 +13,20 @@ const LOGIN_CLIENTS = {
   ember: 0,
 };
 
-let skills = [
-  "Java",
-  "C++",
-  "Photoshop",
-  "Python",
-  "PHP",
-  "MySQL",
-  "word",
-  "excel",
-  "time Mangagement",
-  "finger painting",
-  "under water basket weaving",
-  "fire mechanics",
-  "pro-gamer",
-  "Photography",
-  "speech",
-  "HTML",
-  "CSS",
-  "JavaScript",
-  "Perl",
-  "Assembly"
+let skills = ["Java","C++","Photoshop","Python","PHP","MySQL","word","excel","time Mangagement","finger painting","under water basket weaving","fire mechanics","pro-gamer","Photography","speech","HTML","CSS","JavaScript","Perl","Assembly"]
+//let companies = ["Apple","Microsoft","IBM","Amazon","Google","Cisco","Intel","Oracle","Qualcomm"]
+let companies = 
+[
+  {"description" : "Technology company most known for their phones.", "name" : "Apple", "location" : "Cupertino, California", "_id" : "5a02944ad05618478a1717dj"},
+  {"description" : "Technology company most known for their gaming console.", "name" : "Microsoft", "location" : "Redmond, Washington", "_id" : "5a02944ad05618478a1717dj"},
+  {"description" : "Technology company most known for their work in AI.", "name" : "IBM", "location" : "Armonk, New York", "_id" : "5a02944ad05618478a1717dj"},
+  {"description" : "Technology company most known for AWS and their online shopping website.", "name" : "Amazon", "location" : "Seattle, Washington", "_id" : "5a02944ad05618478a1717dj"},
+  {"description" : "Technology company most known for their search engine.", "name" : "Google", "location" : "Mountain View, California", "_id" : "5a02944ad05618478a1717dj"},
+  {"description" : "Technology company most known for their IoT and computer security services.", "name" : "Cisco", "location" : "San Jose, California", "_id" : "5a02944ad05618478a1717dj"},
+  {"description" : "Technology company most known for their computer chips.", "name" : "Intel", "location" : "Santa Clara, California", "_id" : "5a02944ad05618478a1717dj"},
+  {"description" : "Technology company most known for their database software.", "name" : "Oracle", "location" : "Redwood Shores, California", "_id" : "5a02944ad05618478a1717dj"},
+  {"description" : "Technology company most known for their computer chips", "name" : "Qualcomm", "location" : "San Diego, California", "_id" : "5a02944ad05618478a1717dj"},
+  {"description" : "Technology company most known for their customer relationship management product.", "name" : "SalesForce", "location" : "San Francisco, California", "_id" : "5a02944ad05618478a1717dj"}
 ]
 module.exports = {
   //creates 'client', or user.
@@ -60,5 +53,13 @@ module.exports = {
       name: skills[i]
     });
   }),
+
+  //create 10 companies and populate it into the mongodb.
+  companies: dab.times(10, (i, opts, callback) => {
+    return callback(null, companies[i])
+
+  })
+
+  
 
 };
--- waldo/app/seeds/mongodb/$default.mapping.js
+++ waldo/app/seeds/mongodb/$default.mapping.js
@@ -1,5 +1,6 @@
 'use strict';
 
 module.exports = {
-    skills: 'Skill'
+    skills: 'Skill',
+    companies: 'Company'
 };
