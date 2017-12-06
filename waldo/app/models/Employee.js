509a5e7840f7af86c711861be71d87ae707d04bf
nrhubbar <nrhubbar@iupui.edu>
2017-12-05 16:14:58 -0800

    Updated the models for jobs and employee

--- waldo/app/models/Employee.js
+++ waldo/app/models/Employee.js
@@ -1,14 +1,14 @@
-const mongodb = require ('@onehilltech/blueprint-mongodb');
+const mongodb = require('@onehilltech/blueprint-mongodb');
 
 var schema = new mongodb.Schema({
-    company: {type: String},
-    name: {type: String},
-    phone_Number: {type: String},
-    tite: {type: String},
-    _id: {type: String}
+    companyId: { type: String },
+    name: { type: String },
+    phoneNumber: { type: String },
+    title: { type: String },
+    _id: { type: String }
 });
 
 const COLLECTION_NAME = 'Employees';
 const MODEL_NAME = 'Employee';
 
-module.exports = mongodb.resource (MODEL_NAME, schema, COLLECTION_NAME);
+module.exports = mongodb.resource(MODEL_NAME, schema, COLLECTION_NAME);
--- waldo/app/models/Job.js
+++ waldo/app/models/Job.js
@@ -1,17 +1,16 @@
-const mongodb = require ('@onehilltech/blueprint-mongodb');
+const mongodb = require('@onehilltech/blueprint-mongodb');
 
 var schema = new mongodb.Schema({
-    description: {type: String},
-    company_Logo: {type: String},
-    title: {type: String},
-    contact_Info: {type: String},
-    city: {type: String},
-    state: {type: String},
-    requirements: {type: String},
-    _id: {type: String}
+    description: { type: String },
+    title: { type: String },
+    contactInfo: { type: String },
+    city: { type: String },
+    state: { type: String },
+    requirements: { type: String },
+    _id: { type: String }
 });
 
 const COLLECTION_NAME = 'jobs';
 const MODEL_NAME = 'job';
 
-module.exports = mongodb.resource (MODEL_NAME, schema, COLLECTION_NAME);
+module.exports = mongodb.resource(MODEL_NAME, schema, COLLECTION_NAME);
